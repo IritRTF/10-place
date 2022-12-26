a2659c04763eb2086fc666fcc6ed42960287e7bb
rustam41k0 <rustamskysan@gmail.com>
2022-12-26 12:10:08 +0500

    task complete

--- client/index.mjs
+++ client/index.mjs
@@ -3,22 +3,35 @@ import drawer from "./drawer.mjs";
 import picker from "./picker.mjs";
 
 document.querySelector("#start").addEventListener("submit", e => {
-  e.preventDefault();
-  main(new FormData(e.currentTarget).get("apiKey"));
-  document.querySelector(".container").classList.add("ready");
+    e.preventDefault();
+    main(new FormData(e.currentTarget).get("apiKey"));
+    document.querySelector(".container").classList.add("ready");
 });
 
 const main = apiKey => {
-  const ws = connect(apiKey);
-  ws.addEventListener("message", console.log);
+    const ws = connect(apiKey);
+    ws.addEventListener("message", messageEvent => {
+        let data = JSON.parse(messageEvent.data)
+        console.log(data);
+        console.log(data.payload.place);
+        if (data.type === 'startPlace') drawer.putArray(data.payload.place)
+        else if (data.type === 'updateInServerPlace') drawer.put(data.payload.x, data.payload.y, data.payload.color);
+    });
 
-  timeout.next = new Date();
-  drawer.onClick = (x, y) => {
-    drawer.put(x, y, picker.color);
-  };
+    timeout.next = new Date();
+    drawer.onClick = (x, y) => {
+        ws.send(JSON.stringify({
+            type: 'updatePlace',
+            payload: {
+                x,
+                y,
+                color: picker.color
+            }
+        }));
+    };
 };
 
 const connect = apiKey => {
-  const url = `${location.origin.replace(/^http/, "ws")}?apiKey=${apiKey}`;
-  return new WebSocket(url);
+    const url = `${location.origin.replace(/^http/, "ws")}?apiKey=${apiKey}`;
+    return new WebSocket(url);
 };
